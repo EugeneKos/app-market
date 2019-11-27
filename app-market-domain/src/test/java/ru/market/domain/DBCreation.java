473cf6056db698426fa8dbac7b9de1252300f867
EugeneKos <kosinov4444@yandex.ru>
2019-11-27 11:01:55 +0300

    Added method wich fill user admin.

---
+++ app-market-domain/src/test/java/ru/market/domain/DBCreation.java
@@ -0,0 +1,35 @@
+package ru.market.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import ru.market.domain.config.DomainSpringConfiguration;
+import ru.market.domain.data.dto.UserDTO;
+import ru.market.domain.data.dto.UserWithPasswordDTO;
+import ru.market.domain.data.enums.UserRole;
+import ru.market.domain.service.IUserService;
+import ru.market.domain.utils.DTOUtils;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = DomainSpringConfiguration.class)
+@TestPropertySource("classpath:data-config-main.properties")
+public class DBCreation {
+    @Autowired
+    private IUserService userService;
+
+    @Test
+    public void initDB(){
+        UserWithPasswordDTO dto = DTOUtils.createUserWithPasswordDTO(
+                "Admin", "Admin", "Admin", "admin", "admin"
+        );
+
+        dto.setUserRole(UserRole.ROLE_ADMIN);
+        UserDTO admin = userService.create(dto);
+        Assert.assertNotNull(admin);
+    }
+}

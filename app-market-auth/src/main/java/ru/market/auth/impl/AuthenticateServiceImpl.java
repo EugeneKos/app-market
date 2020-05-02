40111eb778fdc182b10a721cbb73c4a16c3347a4
EugeneKos <kosinov4444@yandex.ru>
2020-05-02 18:27:19 +0300

    MARKET-4: Реализован механизм временной блокировки пользователя.

--- app-market-auth/src/main/java/ru/market/auth/impl/AuthenticateServiceImpl.java
+++ app-market-auth/src/main/java/ru/market/auth/impl/AuthenticateServiceImpl.java
@@ -15,11 +15,17 @@ import ru.market.dto.auth.UsernamePasswordDTO;
 import ru.market.dto.result.ResultDTO;
 import ru.market.dto.result.ResultStatus;
 import ru.market.dto.user.UserAdditionalDTO;
+import ru.market.dto.user.UserStatus;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.UUID;
 
 public class AuthenticateServiceImpl implements AuthenticateService {
     private static final int INACTIVE_INTERVAL = 1200; // 20 min
+    private static final int ALLOWED_PASSWORD_ATTEMPTS = 3;
+    private static final Duration LOCK_TIME = Duration.ofMinutes(15); // 15 min
 
     private IUserService userService;
 
@@ -41,16 +47,44 @@ public class AuthenticateServiceImpl implements AuthenticateService {
 
     @Override
     public Authenticate authenticate(UsernamePasswordDTO usernamePasswordDTO) {
-        sessionDataManager.setUserData(new UserData());
+        initializeUserData();
 
         UserAdditionalDTO userAdditionalDTO = userService.getByUsername(usernamePasswordDTO.getUsername());
         if(userAdditionalDTO == null){
-            return failed("username not found");
+            return failed("Пользователь не найден");
+        }
+
+        UserStatus status = userAdditionalDTO.getStatus();
+        if(status == UserStatus.TEMPORARY_LOCK){
+            LocalDateTime timestampStatus = userAdditionalDTO.getTimestampStatus();
+            Duration duration = Duration.between(timestampStatus, LocalDateTime.now());
+
+            int compare = duration.compareTo(LOCK_TIME);
+            if(compare < 0){
+                Long remainingLockTimeSeconds = LOCK_TIME.getSeconds() - duration.getSeconds();
+                LocalTime remainingLockTime = LocalTime.ofSecondOfDay(remainingLockTimeSeconds);
+
+                return failed(String.format(
+                        "Пользователь временно заблокирован. Осталось %d мин. %d сек.",
+                        remainingLockTime.getMinute(),
+                        remainingLockTime.getSecond()
+                ));
+            }
+
+            userService.updateUserStatusByUsername(
+                    userAdditionalDTO.getUsername(), ru.market.domain.data.enumeration.UserStatus.ACTIVE
+            );
         }
 
         return passwordEncoder.matches(usernamePasswordDTO.getPassword(), userAdditionalDTO.getPassword())
                 ? authenticateSuccess(userAdditionalDTO)
-                : failed("password doesn't match");
+                : failedAuthenticate(userAdditionalDTO);
+    }
+
+    private void initializeUserData(){
+        if(sessionDataManager.getUserData() == null){
+            sessionDataManager.setUserData(new UserData());
+        }
     }
 
     private Authenticate authenticateSuccess(UserAdditionalDTO userAdditionalDTO){
@@ -63,13 +97,39 @@ public class AuthenticateServiceImpl implements AuthenticateService {
         userData.setPersonId(userAdditionalDTO.getPerson().getId());
         userData.setAuthenticate(true);
         userData.setSecretKey(secretKey);
+        userData.setPasswordAttemptCount(0);
 
         sessionManagement.setMaxInactiveInterval(INACTIVE_INTERVAL);
         return new Authenticate(authToken, new ResultDTO(ResultStatus.SUCCESS, "authenticate success"));
     }
 
+    private Authenticate failedAuthenticate(UserAdditionalDTO userAdditionalDTO){
+        UserData userData = sessionDataManager.getUserData();
+
+        if(userData.isAuthenticate()){
+            userData.setAuthenticate(false);
+            userData.setSecretKey(null);
+        }
+
+        int passwordAttemptCount = userData.getPasswordAttemptCount() + 1;
+
+        if(passwordAttemptCount == ALLOWED_PASSWORD_ATTEMPTS){
+            userService.updateUserStatusByUsername(
+                    userAdditionalDTO.getUsername(), ru.market.domain.data.enumeration.UserStatus.TEMPORARY_LOCK
+            );
+
+            userData.setPasswordAttemptCount(0);
+            return failed("Превышено число попыток ввода пароля. Установлена временная блокировка");
+        }
+
+        userData.setPasswordAttemptCount(passwordAttemptCount);
+
+        return failed(String.format(
+                "Пароль не совпадает. Осталось попыток (%d)", ALLOWED_PASSWORD_ATTEMPTS - passwordAttemptCount
+        ));
+    }
+
     private Authenticate failed(String description){
-        sessionManagement.invalidateSession();
         return new Authenticate(null, new ResultDTO(ResultStatus.FAILED, description));
     }
 

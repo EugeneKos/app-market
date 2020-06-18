ea5d59909a21edb77df4a49d5af9efeed0ab8917
EugeneKos <Kosinov4444@yandex.ru>
2020-06-18 23:20:30 +0300

    market/client: Добавлены клиентские api для лимитов и затрат.

---
+++ app-market-client/src/main/java/ru/market/client/rest/CostRestClient.java
@@ -0,0 +1,16 @@
+package ru.market.client.rest;
+
+import ru.market.client.exception.RestClientException;
+
+import ru.market.dto.cost.CostDTO;
+import ru.market.dto.cost.CostNoIdDTO;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+public interface CostRestClient {
+    CostDTO create(CostNoIdDTO costNoIdDTO) throws RestClientException;
+    CostDTO update(CostDTO costDTO) throws RestClientException;
+    Set<CostDTO> getAllByCostLimitIdAndDate(Long costLimitId, LocalDate date) throws RestClientException;
+    void deleteById(Long id) throws RestClientException;
+}

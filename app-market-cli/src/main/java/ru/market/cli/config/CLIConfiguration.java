c02d932b11a073584f2d066a12db069bdbe038ba
EugeneKos <Kosinov4444@yandex.ru>
2020-06-23 16:40:29 +0300

    MARKET-10: Добавлен PropertyLoader и реализация UrlProvider.

--- app-market-cli/src/main/java/ru/market/cli/config/CLIConfiguration.java
+++ app-market-cli/src/main/java/ru/market/cli/config/CLIConfiguration.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import ru.ed.microlib.context.CliManagerApplicationContext;
+
 import ru.market.client.config.ClientConfiguration;
 
 @Configuration
-@Import({ClientConfiguration.class, CliManagerApplicationContext.class})
+@Import({ClientConfiguration.class, CliManagerApplicationContext.class, UrlProviderConfiguration.class})
 @ComponentScan(basePackages = {"ru.market.cli.command"})
 public class CLIConfiguration {
 }
---
+++ app-market-cli/src/main/java/ru/market/cli/utils/PropertyLoader.java
@@ -0,0 +1,27 @@
+package ru.market.cli.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public final class PropertyLoader {
+    private static final String PROPERTIES_FILE_NAME = "config.properties";
+
+    private static Properties properties = new Properties();
+
+    private PropertyLoader(){}
+
+    static {
+        try {
+            String path = System.getProperty("user.dir") + File.separator + PROPERTIES_FILE_NAME;
+            properties.load(new FileInputStream(new File(path)));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String name){
+        return properties.getProperty(name);
+    }
+}
